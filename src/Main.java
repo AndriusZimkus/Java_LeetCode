381a923dfc27b6991536fb536ff9e8989738f388
AndriusZimkus <and.zimkus@gmail.com>
2023-04-18 19:01:45 +0300

    1768 solution

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
+        MergeStringsAlternately1768 mergeStringsAlternately1768 = new MergeStringsAlternately1768();
+
+        System.out.println(mergeStringsAlternately1768.mergeAlternately("abcd", "pq"));
+
     }
 }
---
+++ src/MergeStringsAlternately1768.java
@@ -0,0 +1,23 @@
+public class MergeStringsAlternately1768 {
+    public String mergeAlternately(String word1, String word2) {
+
+        StringBuilder alternatingLetters = new StringBuilder();
+
+        int word1Length = word1.length();
+        int word2Length = word2.length();
+
+        int longerWordLength = (word1Length > word2Length ? word1Length : word2Length);
+
+        for (int i = 0; i < longerWordLength; i++) {
+            if (i < word1Length) {
+                alternatingLetters.append(word1.charAt(i));
+            }
+            if (i < word2Length) {
+                alternatingLetters.append(word2.charAt(i));
+            }
+
+        }
+
+        return alternatingLetters.toString();
+    }
+}
